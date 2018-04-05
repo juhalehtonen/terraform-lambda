02d3d6be5a3738090d61d88ec1ffcea833c39f4b
Juha Lehtonen <juha.t.lehtonen.91@gmail.com>
2018-04-05 23:02:11 +0300

    Move policy JSON to its own document

--- main.tf
+++ main.tf
@@ -50,19 +50,5 @@ resource "aws_lambda_function" "example" {
 resource "aws_iam_role" "lambda_exec" {
   name = "serverless_example_lambda"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+  assume_role_policy = "${file("policies/lambda-role.json")}"
 }
